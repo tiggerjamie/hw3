2e122d7cc8ee1a6c59b6a58a0bc9228baa5c1e59
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-02-24 23:13:32 +0000

    creation of files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "places", :action => "index" })
 
+  resources "places"
+
+resources "entries"
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end

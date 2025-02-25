0bf0680f8c4cf6a89295dd436a03da032c100153
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-02-25 01:46:19 +0000

    indexes, controllers, forms for places and entries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,18 @@
 class EntriesController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    @place = Place.new
+    # assign user-entered form data to places's columns
+    @place["name"] = params["name"]
+    @place["created_at"] = params["created_at"]
+    @place["updated_at"] = params["updated_at"]
+    # save Company row
+    @place.save
+    # redirect user
+    redirect_to "/entry"
+  end
+
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,28 @@
 class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+    @entries = Entry.where({"place_id" => @place["id"]})
+  end
+
+  def new
+    
+  end
+
+  def create
+    @place = Place.new
+    # assign user-entered form data to places's columns
+    @place["name"] = params["name"]
+    @place["created_at"] = params["created_at"]
+    @place["updated_at"] = params["updated_at"]
+    # save Company row
+    @place.save
+    # redirect user
+    redirect_to "/places"
+  end
+
 end

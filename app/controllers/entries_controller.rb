4d6bfa3f7a6f0db57b892ce5b261b2bfb6c747b8
Tigran Tyler Pell <tigran.tylerpell@kellogg.northwestern.edu>
2025-02-25 02:21:58 +0000

    working entries form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,15 +4,22 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @place = Place.new
-    # assign user-entered form data to places's columns
-    @place["name"] = params["name"]
-    @place["created_at"] = params["created_at"]
-    @place["updated_at"] = params["updated_at"]
-    # save Company row
-    @place.save
+    @entry = Entry.new
+    # assign user-entered form data to entry columns
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["created_at"] = params["created_at"]
+    @entry["updated_at"] = params["updated_at"]
+    @entry["place_id"] = params["place_id"]
+    # save entry row
+    @entry.save
+    #update the place update_at
+    place = Place.find_by("id" => @entry["place_id"])
+    place["updated_at"] = Time.current
+    place.save
     # redirect user
-    redirect_to "/entry"
+    redirect_to "/places/#{@entry["place_id"]}"
   end
 
 end
